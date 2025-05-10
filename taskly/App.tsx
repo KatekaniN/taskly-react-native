fe4eefabd8b6d69beed64fb41391e8b877bd40e1
KatekaniN <knyamandi99@gmail.com>
2025-05-10 20:22:03 +0200

    test

--- taskly/App.tsx
+++ taskly/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Katekani Jacqueline Nyamandi</Text>
     </View>
   );
 }
@@ -13,8 +12,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 24,
+    color: "white",
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
