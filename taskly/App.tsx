1df0ce56d4f6eed6c3f141ad6bb71e67965ee945
KatekaniN <knyamandi99@gmail.com>
2025-05-11 13:27:05 +0200

    test

--- taskly/App.tsx
+++ taskly/App.tsx
@@ -4,18 +4,29 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Katekani Jacqueline Nyamandi</Text>
-    </View>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Cofee</Text>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    fontSize: 24,
-    color: "white",
-    backgroundColor: "pink",
     alignItems: "center",
     justifyContent: "center",
   },
+  itemContainer: {
+    paddingHorizontal: 8,
+    borderBottomColor: "#1a759f",
+    borderBottomWidth: 1,
+    backgroundColor: "white",
+    paddingVertical: 16,
+    color: "1a759f",
+  },
+  itemText: {
+    fontSize: 64,
+    fontWeight: 200,
+  }
 });
