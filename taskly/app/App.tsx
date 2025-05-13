3f583dae1191aef6812adcd231d84584b6a55e6e
KatekaniN <knyamandi99@gmail.com>
2025-05-13 19:01:45 +0200

    navigation

--- taskly/app/App.tsx
+++ taskly/app/App.tsx
@@ -1,21 +1,21 @@
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
 import { theme } from "../theme";
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import ShoppingListItem from "../components/ShoppingListItem"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <ShoppingListItem name="coffee" isCompleted />
-      <ShoppingListItem name="tea" />
-      <ShoppingListItem name="sugar" />
-    </View >
-  );
+    return (
+        <View style={styles.container}>
+            <ShoppingListItem name="coffee" isCompleted />
+            <ShoppingListItem name="tea" />
+            <ShoppingListItem name="sugar" />
+        </View >
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
