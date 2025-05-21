7c9a8a970660095f1c7413814b17f2f0bc0c5d3a
KatekaniN <knyamandi99@gmail.com>
2025-05-21 12:12:55 +0200

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,26 @@
 import { Tabs } from "expo-router";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function Layout() {
     return (
         <Tabs >
-            <Tabs.Screen name="index" options={{ title: "Shopping List" }} />
-            <Tabs.Screen name="counter" options={{ title: "Counter" }} />
-            <Tabs.Screen name="idea" options={{ title: "Idea" }} />
+            <Tabs.Screen name="index" options={{
+                title: "Shopping List", tabBarIcon: ({ color, size }) => {
+                    return <Feather name="list" size={size} color={color} />
+                }
+            }} />
+            <Tabs.Screen name="counter" options={{
+                title: "Counter", tabBarIcon: ({ color, size }) => {
+                    return <AntDesign name="pluscircle" size={size} color={color} />
+                }
+            }} />
+            <Tabs.Screen name="idea" options={{
+                title: "Idea", tabBarIcon: ({ color, size }) => {
+                    return <FontAwesome6 name="lightbulb" size={size} color={color} />
+                }
+            }} />
         </Tabs>
     )
 }
--- app/counter.tsx
+++ app/counter.tsx
@@ -1,17 +1,9 @@
 // app/counter.tsx
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
-import { theme } from "../theme";
-import { useRouter } from "expo-router";
 
 export default function CounterScreen() {
-    const router = useRouter();
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={() => router.navigate("/idea")}>
-                <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 48, color: theme.colorCerulean }}>
-                    Go to idea page
-                </Text>
-            </TouchableOpacity>
             <Text style={styles.text}>Counter</Text>
         </View>
     );
